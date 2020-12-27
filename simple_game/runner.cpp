27522002bbf4e886a87dae3523d8f7e43f420494
Mark Liu <uilkram@gmail.com>
2020-12-26 21:31:26 -0800

    cleanups

--- simple_game/runner.cpp
+++ simple_game/runner.cpp
@@ -5,9 +5,12 @@ int main() {
     std::unique_ptr<Game<TTTState, TTTAction>> game = std::make_unique<TicTacToe>();
 
     MCTS<TTTState, TTTAction> mcts;
-    for (int i = 0; i < 1000; i++) {
+
+    // train
+    for (int i = 0; i < 10000; i++) {
         mcts.rollout(game.get());
     }
     mcts.renderTree(/*max_depth=*/3);
+
     return 0;
 };
